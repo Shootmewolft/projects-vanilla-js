5edfc54d1b8eee245a949199e85e1e35ccf970dd
Keyner de Avila <kddeavila3@gmail.com>
2024-09-13 14:25:27 -0500

    v1.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      black: "#161616",
-      white: "#fff",
-      css: "#13A1DF",
-      js: "#F7E018",
-      html: "#FC490B",
+    extend: {
+      colors: {
+        black: "#161616",
+        white: "#fff",
+        css: "#13A1DF",
+        js: "#F7E018",
+        html: "#FC490B",
+      },
+      screens: {
+        xs: "500px",
+      }
     },
   },
   plugins: [],
